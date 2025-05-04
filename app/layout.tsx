7bbb6e4208188093e069530e971b93cc3dc4862a
Ahmadhamdan47 <ahmadhamdan47@gmail.com>
2025-05-04 12:04:05 +0300

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Manufacturer',
+  description: 'Created By Ahmad Hamdan',
+  generator: 'Ahmad Hamdan',
 }
 
 export default function RootLayout({
